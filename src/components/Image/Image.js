4712ecc8a5e132af8920793689ee33b3734dfa7a
Jesper <jespernilsson93@hotmail.se>
2023-04-14 15:15:54 +0200

    fix import of color

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,7 +1,7 @@
 import { Component, createRef, createElement } from 'react'
 import PropTypes from 'prop-types'
 import CircularProgress from '@mui/material/CircularProgress';
-import common from '@mui/material/colors/common'
+import { common } from '@mui/material/colors'
 import BrokenImage from '@mui/icons-material/BrokenImage'
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
